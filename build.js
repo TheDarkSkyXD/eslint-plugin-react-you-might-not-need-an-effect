54565501373d0001413553d2e0e197dbbab1995c
Nick van Dyke <nickjvandyke15@gmail.com>
2025-05-01 17:26:23 -0600

    Fix exporting for CJS

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 import { build } from "esbuild";
 
 build({
-  entryPoints: ["src/index.js"],
+  entryPoints: ["src/index.cjs"],
   bundle: true,
   sourcemap: true,
   format: "cjs",
---
+++ src/index.cjs
@@ -0,0 +1,7 @@
+const youMightNotNeedAnEffectRule = require("./you-might-not-need-an-effect.js");
+
+module.exports = {
+  rules: {
+    "you-might-not-need-an-effect": youMightNotNeedAnEffectRule,
+  },
+};
