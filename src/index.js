9888b4c4fd2a8b2f4d747468d368c38ceebb8d6b
Nick van Dyke <nickjvandyke15@gmail.com>
2025-05-05 06:52:23 -0600

    Fix missing default export for ESM/flat config usage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { name, rule } from "./rule.js";
 
+// Named export for CJS build and/or eslintrc config
 export const rules = {
   [name]: rule,
 };
+// Default export for ESM and/or flat config
+export default { rules };
