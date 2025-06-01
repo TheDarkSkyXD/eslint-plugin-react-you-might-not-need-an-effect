5a5dc9649af87cdb3e5c536c1147790d7d623b74
Nick van Dyke <nickjvandyke15@gmail.com>
2025-06-01 08:32:14 -0600

    Add internal and external tests to initializing state

--- test/initializing-state.test.js
+++ test/initializing-state.test.js
@@ -2,9 +2,25 @@ import { MyRuleTester, js } from "./rule-tester.js";
 import { messageIds } from "../src/messages.js";
 
 new MyRuleTester().run("/initializing-state", {
+  valid: [
+    {
+      name: "Initializing state to external data",
+      code: js`
+        function MyComponent() {
+          const [state, setState] = useState();
+
+          useEffect(() => {
+            fetch("https://api.example.com/data")
+              .then(response => response.json())
+              .then(data => setState(data));
+          }, []);
+        }
+      `,
+    },
+  ],
   invalid: [
     {
-      name: "Initializing state in an effect",
+      name: "Initializing state to literal",
       code: js`
         function MyComponent() {
           const [state, setState] = useState();
@@ -24,6 +40,32 @@ new MyRuleTester().run("/initializing-state", {
           messageId: messageIds.avoidInitializingState,
           data: { state: "state" },
         },
+      ],
+    },
+    {
+      name: "Initializing state to internal data",
+      code: js`
+        function MyComponent() {
+          const [state, setState] = useState();
+          const [otherState, setOtherState] = useState('Meow');
+
+          useEffect(() => {
+            setState(otherState);
+          }, []);
+        }
+      `,
+      errors: [
+        {
+          messageId: messageIds.avoidInternalEffect,
+        },
+        {
+          messageId: messageIds.avoidDerivedState,
+        },
+        // TODO: Should also warn this?
+        // {
+        //   messageId: messageIds.avoidInitializingState,
+        //   data: { state: "state" },
+        // },
       ],
     },
   ],
