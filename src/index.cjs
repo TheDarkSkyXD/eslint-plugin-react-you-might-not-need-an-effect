b14a193333f0d6e685ad5dc1af4e8d69f99c2cd8
Nick van Dyke <nickjvandyke15@gmail.com>
2025-06-03 18:15:07 -0600

    Simplify index.cjs

--- src/index.cjs
+++ src/index.cjs
@@ -1,7 +1,4 @@
-const { name, rule } = require("./rule.js");
-
-module.exports = {
-  rules: {
-    [name]: rule,
-  },
-};
+// `build.js` will bundle everything into CJS.
+// Would be nice to have it use `index.js` directly, but then `esbuild` doesn't
+// seem capable of structuring the CJS export the way ESLint expects.
+module.exports = require("./index.js").default;
