1b1afe8f230d32cd7f6a3b0552cde7d405777f89
Nick van Dyke <nickjvandyke15@gmail.com>
2025-06-08 09:16:15 -0600

    Add recommended @eslint/js config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,28 @@
+import eslint from "@eslint/js";
 import eslintPlugin from "eslint-plugin-eslint-plugin";
 import nodePlugin from "eslint-plugin-n";
+import globals from "globals";
 
 export default [
+  eslint.configs.recommended,
   eslintPlugin.configs["flat/recommended"],
   nodePlugin.configs["flat/recommended"],
   {
     ignores: ["dist/**", "node_modules/**", ".yarn/**"],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["test/**"],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
   },
 ];
